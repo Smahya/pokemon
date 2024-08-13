fc9a01fea03549c69e4181a2249ab6c10b5f7f5d
Salma Salih <salma@enyata.com>
2024-08-13 10:37:37 +0000

    chore: updated modal to handle click outside

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,46 +1,75 @@
 <template>
- <div :class="['main__modal', `main__modal--${props.variant}`]">
-
-  <slot />
-
- </div>
+  <div
+    v-if="open"
+    @click="onClickOutside"
+    :class="['main__modal', `main__modal--${props.variant}`]"
+  >
+    <div class="" @click.stop>
+      <slot />
+    </div>
+  </div>
 </template>
 
 <script setup>
 const props = defineProps({
- variant: {
-  type: String,
-  default: 'center'
- }
+  variant: {
+    type: String,
+    default: "center",
+  },
+  open: {
+    type: Boolean,
+    default: false,
+  },
+});
+const emit = defineEmits(["close"]);
+const onOpen = () => {
+  document.body.style.overflow = "hidden";
+};
+const onClose = () => {
+  document.body.style.overflow = "unset";
+};
+
+const onClickOutside = (e) => {
+  emit("close");
+};
+
+onMounted(() => {
+  onOpen();
+});
+onBeforeUnmount(() => {
+  onClose();
 });
 </script>
 
 <style lang="scss" scoped>
 .main__modal {
- position: fixed;
- left: 0;
- right: 0;
- top: 0;
- bottom: 0;
- background: rgba(39, 39, 39, 0.5);
- backdrop-filter: blur(5px);
- display: flex;
- z-index: 1999;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background: rgba(39, 39, 39, 0.5);
+  backdrop-filter: blur(5px);
+  display: flex;
+  z-index: 1999;
 
+  body {
+    pointer-events: none;
+  }
 
- &--center {
-  align-items: center;
-  justify-content: center;
- }
+  &--center {
+    align-items: center;
+    justify-content: center;
+  }
 
- &--right {
-  align-items: right;
-  justify-content: right;
- }
+  &--right {
+    align-items: right;
+    justify-content: right;
+  }
 
- &--content {
-  background: var(--white);
-  width: 50%;
- }
+  &--content {
+    background: var(--white);
+    width: 50%;
+  }
 }
 </style>
