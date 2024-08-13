6c7177b292bf56cf196c2b91c73a1550b1baf85c
Salma Salih <salma@enyata.com>
2024-08-13 10:35:45 +0000

    chore: added tailwind and updated config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,44 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/color-mode', '@pinia/nuxt',],
+  ssr: false,
+  modules: ["@nuxtjs/color-mode", "@pinia/nuxt", "@nuxtjs/tailwindcss"],
   colorMode: {
-    preference: 'dark', // default value of $colorMode.preference
-    fallback: 'light', // fallback value if not system preference found
-    hid: 'nuxt-color-mode-script',
-    globalName: '__NUXT_COLOR_MODE__',
-    componentName: 'ColorScheme',
-    classPrefix: '',
-    classSuffix: '-mode',
-    storageKey: 'nuxt-color-mode'
+    preference: "dark", // default value of $colorMode.preference
+    fallback: "light", // fallback value if not system preference found
+    hid: "nuxt-color-mode-script",
+    globalName: "__NUXT_COLOR_MODE__",
+    componentName: "ColorScheme",
+    classPrefix: "",
+    classSuffix: "-mode",
+    storageKey: "nuxt-color-mode",
   },
   // ssr: false,
   css: [
     // SCSS file in the project
     "@/assets/css/main.scss",
+    "@/assets/css/tailwind.css",
   ],
   app: {
     head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-      title: 'Salma | Pokemon App',
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Salma | Pokemon App",
       link: [
         {
-          rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/favicon.ico",
         },
         {
-          href: "https://fonts.cdnfonts.com/css/clash-display", rel: "stylesheet"
+          href: "https://fonts.cdnfonts.com/css/clash-display",
+          rel: "stylesheet",
         },
         {
-          href: "https://fonts.cdnfonts.com/css/general-sans", rel: "stylesheet"
-        }
-      ]
+          href: "https://fonts.cdnfonts.com/css/general-sans",
+          rel: "stylesheet",
+        },
+      ],
     },
-    pageTransition: { name: 'page', mode: 'out-in' }
-  }
-})
+    pageTransition: { name: "page", mode: "out-in" },
+  },
+});
